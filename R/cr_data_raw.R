bafc2d890a02fc8b726699c8cc200f26f34679e5
Alex Axthelm <AAxthelm@che.in.gov>
2018-03-18 16:43:06 -0400

    Add function to copy dataset
    will copy from installed location to working directory

--- R/cr_data_raw.R
+++ R/cr_data_raw.R
@@ -4,3 +4,7 @@ cr_data_raw <- system.file(
   "College Readiness with Completion Data 2011-2015.xlsx",
   package = "RWorkshopINAIR2018"
   )
+
+copy_cr_data <- function(to = ".", overwrite = TRUE){
+  file.copy(from = cr_data_raw, to = to, overwrite = overwrite)
+}
