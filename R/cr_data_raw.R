c6be8b19f90723ae8285d3e8c0352aaf69464436
Alex Axthelm <AAxthelm@che.in.gov>
2018-03-18 16:28:42 -0400

    Add function to access data easily

---
+++ R/cr_data_raw.R
@@ -0,0 +1,6 @@
+# See http://r-pkgs.had.co.nz/data.html#data-extdata
+cr_data_raw <- system.file(
+  "extdata",
+  "College Readiness with Completion Data 2011-2015.xlsx",
+  package = "RWorkshopINAIR2018"
+  )
